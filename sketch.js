b55e85111e62a8868f2c7ea7cc82178041e585c0
Jake Welch <94939899+jake-welch-design@users.noreply.github.com>
2024-02-12 15:51:32 -0700

    Update sketch.js
    Little adjustment for stroke offsets

--- sketch.js
+++ sketch.js
@@ -42,7 +42,7 @@ function draw() {
   fill(255);
   noStroke();
   if((windowHeight-windowWidth)/2 >= 0){
-  rect(0,0,windowWidth,(windowHeight-windowWidth)/2);
+  rect(0,0,windowWidth,((windowHeight-windowWidth)/2)+5);
   rect(0,windowHeight - ((windowHeight-windowWidth)/2),windowWidth,(windowHeight-windowWidth)/2);
 }
   pop();
